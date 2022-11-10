64d6beb29768ea9d80b2c5adaef138ec063a4ad6
IamJosephDaramola <daramolaj@gmail.com>
2022-11-10 12:05:14 +0100

    app.ts adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.css'
-import Users from './components/users/Users'
 
 function App() {
   return (
     <div className="App">
       <h1>Learn test</h1>
-      <Users />
     </div>
   )
 }
